b5517cdb7ee05551096319c493c7b975e514f870
Shaggy Bremnath <43152563+shaggy3232@users.noreply.github.com>
2024-07-10 12:22:47 -0400

    more changes

--- pkg/models/availability.go
+++ pkg/models/availability.go
@@ -8,13 +8,15 @@ import (
 
 type Availability struct {
 	gorm.Model
-	ID      uint                `gorm:"primaryKey"`
-	Entries []AvailabilityEntry `json:"entries" gorm:"foreignKey:AvailabilityID"`
+	ID       uint `gorm:"primaryKey"`
+	VendorID uint
+	Entries  []AvailabilityEntry `json:"entries" gorm:"foreignKey:AvailabilityID"`
 }
 
 type AvailabilityEntry struct {
-	ID    uint         `gorm:"primaryKey"`
-	Day   time.Weekday `json:"day"`
-	Start int          `json:"start"`
-	End   int          `json:"end"`
+	ID             uint `gorm:"primaryKey"`
+	AvailabilityID uint
+	Day            time.Weekday `json:"day"`
+	Start          int          `json:"start"`
+	End            int          `json:"end"`
 }
