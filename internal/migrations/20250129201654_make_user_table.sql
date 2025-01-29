73c73b8ca8845bd5f00eb8a38411e01091acb81e
Shaggy Bremnath <43152563+shaggy3232@users.noreply.github.com>
2025-01-29 16:53:47 -0500

    added sql migration with create table querries

---
+++ internal/migrations/20250129201654_make_user_table.sql
@@ -0,0 +1,43 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TYPE role AS ENUM ('ref', 'admin', 'org');
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(11),
+    type role NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL
+
+
+);
+CREATE TABLE availabilities (
+    id UUID PRIMARY KEY,
+    user_id INT REFERENCES users (user_id),
+    price MONEY NOT NULL,
+    start TIMESTAMPTZ NOT NULL,
+    end TIMESTAMPTZ NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL
+);
+CREATE TABLE bookings (
+    id UUID PRIMARY KEY,
+    referee_id INT REFERENCES users (user_id) NOT NULL,
+    organizer_id INT REFERENCES users (user_id) NOT NULL,
+    price MONEY NOT NULL,
+    start TIMESTAMPTZ NOT NULL,
+    end TIMESTAMPTZ NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    accepted BOOL NOT NULL,
+    cancelled BOOL NOT NULl,
+    last_updated TIMESTAMPTZ NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL
+);
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE users;
+DROP TABLE availabilities
+DROP TABLE bookings
+-- +goose StatementEnd
